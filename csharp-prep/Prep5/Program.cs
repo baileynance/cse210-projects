7c572eecc99db4ea53d8e6ca404d7297521918c5
baileynance <baieyj.nance@gmail.com>
2023-09-20 22:53:49 -0700

    prep 5 done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int PromptUserNumber() 
+        {
+            Console.Write("Please enter your favorite number: ");
+            string number = Console.ReadLine();
+            int favNum = int.Parse(number);
+            return favNum;
+        }
+        static int SquareNumber(int num) 
+        {
+            int squareNum = num * num;
+            return squareNum;
+        }
+        static void DisplayResult(string name, int num) 
+        {
+            Console.WriteLine($"{name}, the square of your number is {num}");
+        } 
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(favNumber);
+        DisplayResult(userName, squareNumber);
     }
 }
