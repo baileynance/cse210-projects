7531cbccbc3699d97eb180141d5d89adfa48c2e9
baileynance <baieyj.nance@gmail.com>
2023-09-28 19:24:45 -0700

    finished learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Sales Specialist";
+        job1._company = "Verizon";
+        job1._startYear = 2021;
+        job1._endYear = 2022;;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Assistant Manager";
+        job2._company = "DSG";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Console.WriteLine(job1._company);
+        // Console.WriteLine(job2._company);
+ 
+        // job1.DisplayJob();
+        // job2.DisplayJob();
+     
+        Resume resume1 = new Resume();
+        resume1._name = "Bailey Nance";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+ 
+        // Console.WriteLine(resume1._jobs[0]._jobTitle);
+        // Console.WriteLine(resume1._jobs[1]._jobTitle);
+
+        resume1.DisplayResume();
     }
 }
