8d42260bf8b3eaff8fd9242bf564b86697b49ba0
baileynance <baieyj.nance@gmail.com>
2023-10-12 15:38:54 -0700

    learning 3 completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        static void Fractions(int top, int bottom = 1)
+        {
+            Fraction f = new Fraction();
+            f.SetTop(top);
+            f.SetBottom(bottom);
+            Console.WriteLine(f.GetFractionalString());
+            Console.WriteLine(f.GetDecimalValue());
+        }
+
+        Fractions(1);
+        Fractions(5);
+        Fractions(3, 4);
+        Fractions(1, 3);
     }
 }
