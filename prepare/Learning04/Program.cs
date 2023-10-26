49e048f0cd92795c26c912fd5de5875352c7c097
baileynance <baieyj.nance@gmail.com>
2023-10-26 14:41:02 -0700

    ingeritance prep

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignment = new MathAssignment("Bailey Nance", "Math 101", "7.1", "9-11");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(assignment.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Bailey Nance", "Writing 101", "How to Write a Paper");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
