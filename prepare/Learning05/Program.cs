620da26ef103d22bfdabaac227ca813101a4724d
baileynance <baieyj.nance@gmail.com>
2023-11-09 20:40:43 -0800

    shape done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 3.6));
+        shapes.Add(new Rectangle("blue", 7.4, 4.1));
+        shapes.Add(new Circle("yellow", 5.3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
